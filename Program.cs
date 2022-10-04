1554051530d880020a51eef9161d96e6687586ee
MihaelMilenov <99274377+MihaelMilenov@users.noreply.github.com>
2022-10-04 18:50:33 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _03._2022_УООП
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
